2694623ae7e26a22aaef7e73449516958dd4da77
Jordy Manner <jordy.manner@milkcreation.fr>
2016-10-26 20:59:57 +0000

    tiFyCore - vendor/Lib/File ImportRemoteAttachment ...

--- presstify/vendor/tiFy/File.php
+++ presstify/vendor/tiFy/File.php
@@ -20,16 +20,8 @@ class File
 				$filename = preg_replace( '/^'. preg_quote( site_url( '/' ), '/' ) .'/', tiFy::$AbsPath, $filename );
 				$contents = file_get_contents( $filename );
 			else :
-				$ch = curl_init();
-	
-				curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
-				curl_setopt($ch, CURLOPT_HEADER, 0);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				curl_setopt($ch, CURLOPT_URL, $filename);
-				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-			
-				$contents = curl_exec($ch);
-				curl_close($ch);
+				$response = wp_remote_get( $filename );			
+				$contents = wp_remote_retrieve_body( $response );
 			endif;
 		elseif( file_exists( tiFy::$AbsPath . ltrim( $filename ) ) ) :
 			$contents = file_get_contents( tiFy::$AbsPath . ltrim( $filename ) );
@@ -40,4 +32,100 @@ class File
 		return $contents;
 	}
 	
+	
+	/**
+	 * Import de fichier local ou distant dans le repertoire d'upload
+	 */
+	public static function importMedia( $filename, $name = null, $time = null, $max_size = 0 )
+	{
+		if( ! $name )
+			$name = basename( $filename );
+		
+		$response = wp_remote_get( $filename );
+		
+		// 
+		if ( ! $response )
+			return new \WP_Error( 'tify_file_import_media_error', __( 'Le serveur distant ne répond pas', 'tify' ) );
+		
+		//
+		$code = wp_remote_retrieve_response_code( $response );
+		$message = wp_remote_retrieve_response_message( $response );
+		if( $code != '200' )
+			return new \WP_Error( 'tify_file_import_media_error', sprintf( __( 'Le serveur distant a retourné l\'erreur suivante : %1$d %2$s', 'tify' ), esc_html( $message ), $code ) );
+
+		$body = wp_remote_retrieve_body( $response );	
+		$upload = wp_upload_bits( $name, 0, $body );
+		
+		$filesize = filesize( $upload['file'] );
+		$content_length = wp_remote_retrieve_header( $response, 'content-length' );
+		if ( $content_length  && ( $filesize != $content_length ) ) :
+			@unlink( $upload['file'] );
+			return new \WP_Error( 'tify_file_import_media_error', __('La taille du fichier distant est incorrect', 'tify' ) );
+		endif;
+
+		if ( 0 == $filesize ) :
+			@unlink( $upload['file'] );
+			return new \WP_Error( 'tify_file_import_media_error', __( 'Le fichier téléchargé est vide', 'tify' ) );
+		endif;
+
+		if ( ! empty( $max_size ) && $filesize > $max_size ) :
+			@unlink( $upload['file'] );
+			return new \WP_Error( 'tify_file_import_media_error', sprintf(__( 'Le fichier distant est trop lourd, la limite est fixée à %s', 'tify' ), size_format( $max_size ) ) );
+		endif;
+		
+		return $upload;	
+	}
+	
+	/** 
+	 * Import de fichier local ou distant en tant que fichier attaché
+	 */
+	public static function importAttachment( $filename, $postdata = array(), $name = null, $time = null, $max_size = 0 )
+	{
+		$media = self::importMedia( $filename, $name, $time, $max_size );
+		
+		if( is_wp_error( $media ) )
+			return $media;
+		
+		$file = $media['file'];
+		
+		// Traitement des arguments du fichier attaché
+		$attachment = array_merge(
+			array(
+				'post_mime_type' 	=> $media['type'],
+				'guid' 				=> $media['url'],
+				'post_parent' 		=> 0,
+				'post_title'		=> sanitize_title( basename( $media['file'] ) ),
+				'post_content' 		=> '',
+				'post_excerpt' 		=> ''
+			),
+			$postdata	
+		);
+		$attachment_id = wp_insert_attachment( $attachment, $file );
+		
+		if ( ! is_wp_error( $attachment_id ) ) :
+			wp_update_attachment_metadata( $attachment_id, wp_generate_attachment_metadata( $attachment_id, $file ) );
+		endif;
+		
+		return $attachment_id;
+	}
+	
+	/** == == **/
+	public static function getAttachmentDatas( $attachment_id )
+	{
+		if( ! $post = get_post( $attachment_id ) )
+			return;
+		
+		if( $post->post_type !== 'attachment' )
+			return;
+			
+		return array(
+			'ID'			=> $attachment_id,
+			'title'			=> get_the_title( $attachment_id ),
+			'content'		=> $post->post_content,
+			'excerpt'		=> get_the_excerpt( $attachment_id ),	
+			'url'			=> wp_get_attachment_url( $attachment_id ),
+			'upload'		=> tify_upload_url( $attachment_id ),
+			'icon'			=> wp_get_attachment_image( $attachment_id, array( 80, 60 ), true )
+		);
+	}
 }
