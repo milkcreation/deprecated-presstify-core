e2ab15ddb6eb0d21190ee48bfd0a85bd545aa8d7
Jordy Manner <jordy.manner@milkcreation.fr>
2016-11-30 14:51:36 +0000

    tiFyCore : Forms/Addons/Record : Mise en place de l'affichage en ligne des enregistrements de formulaire

---
+++ presstify/core/Forms/Addons/Record/ListTable.js
@@ -0,0 +1,37 @@
+jQuery(document).ready( function($){
+
+	$( document ).on( 'click', '#the-list .row-actions a.inline-preview', function(e){
+		e.preventDefault();
+		
+		var vars = {}; 
+		$(this).attr('href').replace( 
+			/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+			function( m, key, value ) { // callback
+				vars[key] = value !== undefined ? value : '';
+			}
+		);
+		if( vars['ID'] === undefined )
+			return;
+		var record_id = parseInt( vars['ID'] );
+		
+		var $parent = $(this).closest( 'tr' );
+		
+		if( $parent.next().attr('id') != 'inline-preview-'+ record_id ){
+			// Création de la zone de prévisualisation
+			$previewRow = $( '#inline-preview' ).clone(true);
+			$previewRow.attr( 'id', 'inline-preview-'+record_id );
+			$parent.after( $previewRow );
+			// Récupération des éléments de formulaire
+			$.post( tify_ajaxurl, { action: 'tiFyCoreFormsAddonsRecordListTableInlinePreview', record_id: record_id }, function( data ){
+				console.log( data );
+				$('> td', $previewRow ).html(data);			
+			});					
+		} else {
+			$previewRow = $parent.next();
+		}	
+		$parent.closest('table');
+		$previewRow.toggle();	
+				
+		return false;
+	});
+});
