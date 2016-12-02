2e33197c6c6f8fea4addff5a6741b274f17a686c
mastermilk <mastermilk@83c550a3-a5a7-4543-ba4f-90be383028a9>
2016-12-02 15:00:52 +0000

    roomtech.fr - mise à jour de l'info de révision

--- core/trunk/presstify/tiFy.php
+++ core/trunk/presstify/tiFy.php
@@ -5,7 +5,7 @@ final class tiFy
 {
 	/* = ARGUMENTS = */
 	// Version de PresstiFy
-	public static $Version			= '1.0.148';
+	public static $Version			= '1.0.149';
 
 	// Chemins absolue vers la racine de l'environnement
 	public static $AbsPath;
