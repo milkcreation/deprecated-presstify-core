f297b6d1dfe1879edf91fc47f5cbf5c911167cf0
mastermilk <mastermilk@83c550a3-a5a7-4543-ba4f-90be383028a9>
2016-12-15 08:08:33 +0000

    tiFyCore - correctif bug ContactForm

--- core/trunk/presstify/tiFy.php
+++ core/trunk/presstify/tiFy.php
@@ -5,7 +5,7 @@ final class tiFy
 {
 	/* = ARGUMENTS = */
 	// Version de PresstiFy
-	public static $Version			= '1.0.184';
+	public static $Version			= '1.0.185';
 
 	// Chemins absolue vers la racine de l'environnement
 	public static $AbsPath;
