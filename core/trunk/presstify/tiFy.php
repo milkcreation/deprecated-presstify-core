ea016daa35a264b7eece279d1d406bfa3683c68d
mastermilk <mastermilk@83c550a3-a5a7-4543-ba4f-90be383028a9>
2017-01-10 19:01:29 +0000

    tiFyCore - Ajout Vendor MobileDetect r205

--- core/trunk/presstify/tiFy.php
+++ core/trunk/presstify/tiFy.php
@@ -5,7 +5,7 @@ final class tiFy
 {
 	/* = ARGUMENTS = */
 	// Version de PresstiFy
-	public static $Version			= '1.0.198';
+	public static $Version			= '1.0.205';
 
 	// Chemins absolue vers la racine de l'environnement
 	public static $AbsPath;
