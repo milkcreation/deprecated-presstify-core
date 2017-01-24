f51b83a90565657abadd370844583baafbae5f4f
mastermilk <mastermilk@83c550a3-a5a7-4543-ba4f-90be383028a9>
2017-01-24 12:51:20 +0000

    tiFyCore - Correctif Components/CustomFields/Permalink - post_id === permalink_post_id

--- core/trunk/presstify/tiFy.php
+++ core/trunk/presstify/tiFy.php
@@ -5,7 +5,7 @@ final class tiFy
 {
 	/* = ARGUMENTS = */
 	// Version de PresstiFy
-	public static $Version			= '1.0.213';
+	public static $Version			= '1.0.218';
 
 	// Chemins absolue vers la racine de l'environnement
 	public static $AbsPath;
