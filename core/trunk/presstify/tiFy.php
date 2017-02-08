2857a353cd3f4f8ac098e3bdca513338050681d9
mastermilk <mastermilk@83c550a3-a5a7-4543-ba4f-90be383028a9>
2017-02-08 10:30:19 +0000

    tiFyCore - Ajustements ComponentsLogin

--- core/trunk/presstify/tiFy.php
+++ core/trunk/presstify/tiFy.php
@@ -5,7 +5,7 @@ final class tiFy
 {
 	/* = ARGUMENTS = */
 	// Version de PresstiFy
-	public static $Version			= '1.0.227';
+	public static $Version			= '1.0.228';
 
 	// Chemins absolue vers la racine de l'environnement
 	public static $AbsPath;
