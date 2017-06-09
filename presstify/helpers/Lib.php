d136d3821d8459d95f131c80bc44d54a794928c2
Jordy Manner <jordy.manner@milkcreation.fr>
2017-06-09 08:02:26 +0000

    presstiFy : Suppression du component Video Création de la Lib Video Possibilité d'afficher une vidéo en mode inline

--- presstify/helpers/Lib.php
+++ presstify/helpers/Lib.php
@@ -14,16 +14,29 @@ namespace
 	{
 		return tiFy\Lib\Modal\Modal::display( $args, $echo );
 	}
-	
-	/** == Création d'un contrôleur d'affichage d'une modale video == **/
-	function tify_modal_video_toggle( $args = array(), $echo = true )
+    // --------------------------------------------------------------------------------------------------------------------------
+	/* = VIDÉO = */
+    /** == Création d'un contrôleur d'affichage d'une modale video == **/
+	function tify_video_modal_toggle( $args = array(), $echo = true )
 	{
-		return tiFy\Lib\Modal\Video::toggle( $args, $echo );
+	    return \tiFy\Lib\Video\Modal::toggle( $args, $echo );
 	}
 	
 	/** == Création d'une modale vidéo == **/
-	function tify_modal_video( $args = array(), $echo = true  )
+	function tify_video_modal( $args = array(), $echo = true )
 	{
-		return tiFy\Lib\Modal\Video::display( $args, $echo );
+	    return \tiFy\Lib\Video\Modal::display( $args, $echo );
+	}
+	
+	/** == Création d'un contrôleur d'affichage d'une vidéo en ligne == **/
+	function tify_video_inline_toggle( $target = null, $args = array(), $echo = true )
+	{
+	    return \tiFy\Lib\Video\Inline::toggle( $target, $args, $echo);
+	}
+	
+	/** == Affichage d'une vidéo en ligne == **/
+	function tify_video_inline( $attr, $echo = true )
+	{
+	    return \tiFy\Lib\Video\Inline::display( $attr, $echo );
 	}
 }
