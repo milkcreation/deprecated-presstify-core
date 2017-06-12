6da9e7d66e0a6397824d752ebe2e624c869e56ea
mastermilk <mastermilk@83c550a3-a5a7-4543-ba4f-90be383028a9>
2017-06-12 07:48:09 +0000

    presstiFy - Mise à jour Core/Security > htaccess.txt

--- core/trunk/presstify/tiFy.php
+++ core/trunk/presstify/tiFy.php
@@ -6,7 +6,7 @@ final class tiFy
     /**
      * Version de PresstiFy
      */ 
-    public static $Version            = '1.0.324';
+    public static $Version            = '1.0.325';
     
     /**
      * Chemins absolue vers la racine de l'environnement
