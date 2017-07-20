9764f1140b20275a02837684cc1b56715bac1c4d
Jordy Manner <jordy.manner@milkcreation.fr>
2017-07-20 13:05:56 +0000

    PresstiFy assets -> minification Core/Control/Suggest - attr select gestion valeur booléenne

--- presstify/tiFy.php
+++ presstify/tiFy.php
@@ -6,7 +6,7 @@ final class tiFy
     /**
      * Version de PresstiFy
      */ 
-    public static $Version            = '1.0.355';
+    public static $Version            = '1.0.356';
     
     /**
      * Chemins absolue vers la racine de l'environnement
